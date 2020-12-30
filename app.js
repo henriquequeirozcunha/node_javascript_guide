0a011088abed3ddf53abc8c7069a79d408de4280
Henrique Cunha <henrique.cunha@corpus.inf.br>
2020-12-30 16:53:55 -0300

    feat: define cart and cart-item models and configure the relationship inside app.js

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,6 +28,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 const seedData = async () => {
     const userAdmin = await User.findAll({
@@ -42,7 +48,7 @@ const seedData = async () => {
 
 const startApp = async () => {
   try {
-    const connection = await sequelize.sync();
+    const connection = await sequelize.sync({force: true});
     await seedData();
     app.listen(3000);
     console.log('app listening to port 3000');
